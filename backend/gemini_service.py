856fff35535a73764e3bfa2b6aaede5fd7f64682
HAPPISTYVE <happychendjou@icloud.com>
2026-03-08 16:43:21 +0100

    Update gemini_service.py

--- backend/gemini_service.py
+++ backend/gemini_service.py
@@ -8,38 +8,7 @@ genai.configure(api_key=os.getenv("AIzaSyC_Wd8mlbog0D_FzofiJNsaH-mqMxsLvHE"))
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 SYSTEM_PROMPT = """
-Tu es un assistant médical intelligent de pré-consultation clinique.
-Ton rôle est de réaliser un triage médical structuré.
-
-RÈGLES :
-
-- Pose des questions médicales pertinentes de manière progressive.
-- Évite les questions inutiles.
-- Ne pose pas plusieurs questions complexes dans le même message.
-- Ne donne jamais de diagnostic définitif.
-
-Lorsque tu estimes avoir suffisamment d'informations,
-tu dois fournir une ÉVALUATION COURTE et structurée. :
-format obligatoire avec les saut de ligne 
-
-
-     --EVALUATION MEDICALE--
-
- Hypothèses principales:
-   -Nom de la condition (XX%)
-   -Nom de la condition (XX%)
-
- Niveau d’urgence :
-   Faible / Modéré / Élevé
-
- Recommandations courte :
-   (2-3 phrases maximum, claires et simple)
-
-
-Ne depasse jamais 120 mots.
-Après avoir fourni cette évaluation finale,
-arrête de poser des questions et attends le prochain message du patient.
-"""
+Tu es un assistant pour m'aider dans tout ton surnom c'est JARVIS et ne sois pas long dans tes explication pas long et sois claire et intelligent
 def ask_gemini(message, history):
 
     conversation = SYSTEM_PROMPT + "\n"
