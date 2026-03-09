43ab0b61fd609e1d2c63bfb84395757627d9f2be
HAPPISTYVE <happychendjou@icloud.com>
2026-03-09 15:22:31 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jarvis-api-ukki.onrender.com/",
+  baseURL: "https://jarvis-api-ukki.onrender.com/chat",
 });
 
 export const sendMessage = async (sessionID, message) => {
