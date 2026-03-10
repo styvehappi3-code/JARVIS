9cb25b5b313516d3adc67986ba50273742fdd8d7
HAPPISTYVE <happychendjou@icloud.com>
2026-03-10 22:57:07 +0100

    Update groq_service.py

--- backend/groq_service.py
+++ backend/groq_service.py
@@ -1,7 +1,7 @@
 from groq import Groq
 import os
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=os.getenv("Gsk_MVDMnfJZpoagJYsT6TDDWGdyb3FY8p7Kz4n1ciDKbYVv98e4wwoS"))
 
 def ask_ai(message, history):
 
