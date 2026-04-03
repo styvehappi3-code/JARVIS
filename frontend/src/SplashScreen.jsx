19417c7e125e0a33e447d3e8bbf9a36759c59bff
HAPPISTYVE <happychendjou@icloud.com>
2026-04-03 01:03:12 +0100

    Update SplashScreen.jsx

--- frontend/src/SplashScreen.jsx
+++ frontend/src/SplashScreen.jsx
@@ -1,1 +1,28 @@
+import animation from "./assets/Jarvis_interface.webp";
 
+function SplashScreen() {
+  return (
+    <div style={styles.container}>
+      <img src={animation} alt="loading" style={styles.image} />
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "#000",
+    zIndex: 9999,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover", // 🔥 ÇA c’est la clé du full screen
+  },
+};
+
+export default SplashScreen;
